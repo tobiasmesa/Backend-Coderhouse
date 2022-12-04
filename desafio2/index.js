fb7e078361b382b340be96b79667ed2cade756c1
tobiasmesa <tobiasmesa03@gmail.com>
2022-12-04 13:15:22 -0300

    Entrega del desafio numero 2

--- desafio2/index.js
+++ desafio2/index.js
@@ -1,27 +1,36 @@
 import { existsSync, readFileSync, writeFileSync } from "fs";
 
-const { log } = console;
-
 class ProductManager {
-  #readFile() {
-    return JSON.parse(readFileSync(this.path));
-  }
-  #writeProducts(products) {
-    return writeFileSync(this.path, JSON.stringify(products));
-  }
   constructor(filePath) {
-    this.path = filePath;
-    if (existsSync(this.path)) {
+    this.path = filePath; 
+    if (existsSync(this.path)) { // Take path and ask if it exists
       this.#readFile();
-    } else {
+    } else { // else write a document
       this.#writeProducts([]);
     }
   }
+  
+  
+  /**
+   * @returns json from path
+   */
+  #readFile() {
+    return JSON.parse(readFileSync(this.path)); // Read the document in the given path
+  }
+  /**
+  * @desc
+   * @param {*} products
+   * @returns
+   */
+  #writeProducts(products) {
+    return writeFileSync(this.path, JSON.stringify(products)); // Write a doc with products array on it
+  }
+  
 
   addProduct(title, description, price, thumbnail, code, stock) {
-    const products = this.#readFile();
-    const findCode = products.find((product) => product.code === code);
-    if (!findCode) {
+    const products = this.#readFile(); // Take products from file
+    const findCode = products.find((product) => product.code === code); // Look for a product with same code given 
+    if (!findCode) { // If it's ok, add product to array
       products.push({
         title,
         description,
@@ -31,13 +40,13 @@ class ProductManager {
         stock,
         id: this.#getMaxId() + 1,
       });
-      this.#writeProducts(products);
+      this.#writeProducts(products); // Save it on file
       return "product added";
     }
     return "product code already exist";
   }
 
-  #getMaxId() {
+  #getMaxId() { // Id generator
     let maxId = 0;
     this.#readFile().map((product) => {
       if (product.id > maxId) maxId = product.id;
@@ -46,10 +55,10 @@ class ProductManager {
   }
 
   getProducts() {
-    return this.#readFile();
+    return this.#readFile(); // Get products from file
   }
 
-  getProductById(id) {
+  getProductById(id) { // Get products with same ID on file
     const product = this.#readFile().find((product) => product.id === id);
     if (product) {
       return product;
@@ -58,19 +67,6 @@ class ProductManager {
   }
 
 
-  updateProduct(id, key, value) {
-    const products = this.#readFile();
-    const productIndex = products.findIndex((product) => product.id === id);
-    if (productIndex !== -1) {
-      const productUpdated = (products[productIndex][key] = value);
-      log(productUpdated);
-      this.#writeProducts(productUpdated);
-    } else {
-      return log("Product doesn't exist");
-    }
-  }
-
-  
   updateProduct(id, key, value) {
     let products = this.#readFile();
     const product = products.find((producto) => producto.id === id);
@@ -81,7 +77,6 @@ class ProductManager {
     this.#writeProducts(products);
     console.log("Producto Actualizado");
   }
-
 
   deleteProduct(id) {
     const products = this.#readFile();
@@ -94,13 +89,32 @@ class ProductManager {
   }
 }
 
+const pm = new ProductManager("./products.json");
 
-const pm = new ProductManager('./products.json')
+console.log(pm.getProducts());
+console.log(
+  pm.addProduct(
+    "producto prueba",
+    "Este es un producto prueba",
+    200,
+    "Sin imagen",
+    "ab123",
+    25
+  )
+);
+console.log(
+  pm.addProduct(
+    "producto prueba 2",
+    "Este es un producto prueba 2",
+    200,
+    "Sin imagen",
+    "ac2313",
+    221
+  )
+);
+console.log(pm.getProductById(1));
+console.log(pm.getProductById(4));
+console.log(pm.updateProduct(2, "name", "producto 2"));
+console.log(pm.deleteProduct(1));
 
-console.log(pm.getProducts())
-console.log(pm.addProduct('producto prueba', "Este es un producto prueba", 200, "Sin imagen", "ab123", 25))
-console.log(pm.addProduct('producto prueba 2', "Este es un producto prueba 2", 200, "Sin imagen", "ac2313", 221))
-console.log(pm.getProductById(1))
-console.log(pm.getProductById(4))
-console.log(pm.updateProduct(2, "name", "producto 2"))
-console.log(pm.deleteProduct(1))
+export default { ProductManager };
